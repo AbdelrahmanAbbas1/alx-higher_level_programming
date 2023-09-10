2b648237824e24bec5bf8478227d760b91217326
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-09-10 17:16:57 +0000

    Adding new file

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    res = 1
+    if b > 0:
+        for i in range(b):
+            res = res * a
+    elif b < 0:
+        for i in range(-b):
+            res = res * (1 / a)
+    else:
+        res = 1
+    return res
