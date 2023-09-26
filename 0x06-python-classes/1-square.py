2dd56b9947cae2d2c3401fafdb3ef76121d4a786
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-09-26 22:32:31 +0000

    Adding new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a Square"""
+
+
+class Square:
+    """Represent a squre"""
+
+    def __init__(self, size):
+        """Initializes a new Square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
