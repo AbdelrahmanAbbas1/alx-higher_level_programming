e707b548c81185a04ebbf6b3181e447ffc8cdb2d
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-09-26 23:26:38 +0000

    Adding new file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+3-square.py
+Define a Square
+"""
+
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size fo the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return the area of the new square"""
+        return self.__size**2
+
+    def my_print(self):
+        """Prints the new square with #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
