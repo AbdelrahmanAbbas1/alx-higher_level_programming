ae6dda1cf6f01055773abd7b7d46860292fb3ffe
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-09-27 00:19:15 +0000

    Adding new file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,7 +51,7 @@ class Square:
         if self.__size == 0:
             print()
             return
-        print("\n" * self.__position[1], end="")
+        [print("") for i in range(0, self.__position[1])]
         for i in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
