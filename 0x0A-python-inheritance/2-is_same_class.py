53f760db7deedecfdd5cf85e955ba9b54add62ea
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-10-15 10:01:24 +0000

    Adding new file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+
+
+def is_same_class(obj, a_class):
+    """Checks the instanceof the class
+
+    Args:
+        obj (obj): An instance of the class
+        class (class): The class
+
+    Return:
+        True if the obj is instance and false otherwise
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
