0bfa683eb63c723b37aa688f55da7c47dd6ff661
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-10-16 05:30:13 +0000

    Adding new file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This Module defines a class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A subclass representing a Square"""
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
