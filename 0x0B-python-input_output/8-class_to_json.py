ae849b175f20d283fe57ac10cd36b892180c83d5
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-08 22:47:51 +0000

    Adding new file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+import json
+
+
+def class_to_json(obj):
+    """This function returns the dictionary description with
+        simple data structure
+
+        Args:
+            obj (obj): The object to be turned to JSON serialization
+
+        Return:
+            Returns the dictionary description
+    """
+    return obj.__dict__
