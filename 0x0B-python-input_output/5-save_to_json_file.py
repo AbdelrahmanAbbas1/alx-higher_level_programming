512577bad3f38d5bd8fb65a9f33831565134a1c7
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-08 15:10:10 +0000

    Adding new file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function writes an object to a text file
+
+        Args:
+            my_obj (obj):
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
