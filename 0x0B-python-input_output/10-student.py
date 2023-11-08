de67eae29a374c0feb75096b347404ff1758f2e8
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-08 23:13:06 +0000

    Adding new file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a class"""
+
+
+class Student():
+    """This class represents a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
