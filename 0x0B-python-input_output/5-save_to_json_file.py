53450eee69e07aad9a22c2eb1721c04c2892a9ca
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-08 16:04:49 +0000

    Adding new file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
         Args:
             my_obj (obj):
     """
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
