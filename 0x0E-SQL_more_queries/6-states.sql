1819cad448b9c09e40e73163d90e94d2bc28b028
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-19 23:41:13 +0000

    Adding new file

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates teh database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256));
