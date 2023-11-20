f3fca4abecbafcd57e0d57ef14e199b8f1d0012d
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-11-20 00:04:26 +0000

    Adding new file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Lists all the cities if California that can be found in the database
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM states
 	WHERE name = 'California'
-);
-ORDER BY id ASC
+)
+ORDER BY id ASC;
