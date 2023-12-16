baec7c0861b8c3fe4f7542f9ea4ba3d8c0dfdd55
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-12-16 22:04:42 +0000

    Adding new file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all states where name matches the argument"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2],
+                         sys.argv[3], 3306)
+    cur = db.cursor()
+    cur.execute("""
+                SELECT * FROM states WHERE name = '{}'
+                ORDER BY states.id
+                """.format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
