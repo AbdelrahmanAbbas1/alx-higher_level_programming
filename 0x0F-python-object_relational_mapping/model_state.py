d8cd505cbb7b8a22270df2849ac8d163345b3882
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-12-20 19:31:11 +0000

    Adding new file

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class of a State and an instance of Base"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, unique=True,
+                nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
