00b6dd5558aaf37132677090fa35dd8f370fe5b0
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-02-21 17:32:33 +0000

    Adding new file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,16 @@
-ii
+#!/usr/bin/python3
+"""Lists all states from the database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states')
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
