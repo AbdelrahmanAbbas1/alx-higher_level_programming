aa43831dce812ce8a874c147de287d4d550bec1e
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-02-24 14:48:42 +0000

    Adding new file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class State"""
+"""Lists all states from the database hbtn_0e_6_usa"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
@@ -14,3 +14,4 @@ if __name__ == '__main__':
 
     for state in session.query(State):
         print('{}: {}'.format(state.id, state.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Prints the first state object from hbtn_0e_6_usa"""
+"""Lists all states objects the contain letter a"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).first()
-    if state is not None:
+    for state in session.query(State).filter(State.name.like('%a%')):
         print('{}: {}'.format(state.id, state.name))
-    else:
-        print('Nothing')
     session.close()
