d87b3750a49737eecd2de197296b738b8389ccd0
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-04-21 21:53:50 +0000

    Adding new file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request
-curl -X POST -H "email: test@gmail.com; subject: I will always be here for PLD" -s "$1"
+curl -X POST -d "email: test@gmail.com&subject: I will always be here for PLD" -s "$1"
