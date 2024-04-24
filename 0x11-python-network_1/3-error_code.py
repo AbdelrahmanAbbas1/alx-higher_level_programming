5d796b8469b6da07d6943fcff7cdfa30cf8e1510
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-04-24 18:06:22 +0000

    Adding new file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script displays the body of response of the passed url"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(e.code)
