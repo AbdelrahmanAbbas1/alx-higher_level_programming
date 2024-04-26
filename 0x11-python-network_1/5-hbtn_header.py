3b2025c8f82053ce0f989605dd12d36037074e4f
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-04-26 15:01:55 +0000

    Adding new file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script displays the value of a header variable of url"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.headers["X-Request-Id"])
