1d2db4758cc93ca198b69a6a128e085893f4aa93
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-04-28 14:45:27 +0000

    Adding new file

---
+++ 0x11-python-network_1/100-guthub_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script lists 10 most recent commits or the repo"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    res = requests.get(url)
+    resjson = res.json()
+    for i in range(0, 10):
+        sha = resjson[i].get("sha")
+        author = resjson[i].get("commit").get("author").get("name")
+        date = resjson[i].get("commit").get("author").get("date")
+        print(f"{sha}: {author}")
