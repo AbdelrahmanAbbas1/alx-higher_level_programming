39419674a31f1e6625c6d1795d00c8c7620be55d
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-09-25 11:21:33 +0000

    Adding new file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const url = process.argv[2];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
 
+request(url, (err, response, data) => {
+  if (err) console.log(err);
 
+  const films = JSON.parse(data);
+  let count = 0;
+  films.results.forEach(film => {
+    film.characters.forEach(ch => {
+      if (ch === character) count++;
+    });
+  });
+  console.log(count);
+});
